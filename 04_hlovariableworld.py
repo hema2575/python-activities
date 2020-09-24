4f6c18ecd65e75110c25ee7064e42a66d9e9a7b9
chilli <70721729+hema2575@users.noreply.github.com>
2020-09-23 20:29:57 -0500

    I did a few print commands

---
+++ 04_hlovariableworld.py
@@ -0,0 +1,10 @@
+name = "Josh"
+country = "USA"
+age = 25
+hourly_wage = 100
+satisfied = True
+daily_wage = hourly_wage * 8
+
+print("Name is:" + name + " Country is:" + country + " Age is: " + str(age) + " hourly_wage is: " + str(hourly_wage))
+
+print(f"Name is: {name} Country is: {country} Age is: {age} and hourly_wage is {hourly_wage}")
