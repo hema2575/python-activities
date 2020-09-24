b344f3fe3c700f5e7dd86d60f2b7a66c14bae611
chilli <70721729+hema2575@users.noreply.github.com>
2020-09-24 16:53:25 -0500

    for loops

---
+++ 21_simple_loops.py
@@ -0,0 +1,22 @@
+for x in range(10):
+    print(x)
+    
+# If two numbers are provided then a For loop will loop from the first number up until it reaches the second number
+for x in range(20,30):
+    print(x)
+    
+# If a list is provided, then the For loop will loop through each element within the list    
+words = ["Peanut", "Butter", "Jelly", "Time", "Is", "Now"]
+for word in words:
+    print(word)
+    
+# A While Loop will continue to loop through the code contained within it until some condition is met
+
+x = "yes"
+while x == "yes":
+    print("Merry go rounds are great")
+    x = input("would you like to go on merrygorounds again? ")
+    
+
+     
+    
