18a8b08acef3d51ac30173f0fb22f91b2b2dc4fe
chilli <70721729+hema2575@users.noreply.github.com>
2020-09-23 19:46:04 -0500

    added variables.py

---
+++ 03_variables.py
@@ -0,0 +1,21 @@
+#x='hello world'
+name =" hs"
+age = 10
+
+title = "Frankfurter"
+years = 80
+expert_status=True
+
+print("Nick is a professional " + title)
+
+print("Nick has been coding for  "+str(years) + " years")
+
+print ("Expert_status: " + str(expert_status))
+
+print(f"Expert_status: {expert_status}")
+
+print(f"{name} -I learnt to use print-f, so I will use it at {years} years")
+
+print(f"My name is {name} and my age is {age}")
+#print (x)
+
